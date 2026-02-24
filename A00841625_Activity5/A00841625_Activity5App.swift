bcb52f58b867b58f396461a0c1ee1b7eefe6e052
Adan Flores <adan.gonzalez@enviaflores.com>
2026-02-24 12:08:58 -0600

    Initial Commit

---
+++ A00841625_Activity5/A00841625_Activity5App.swift
@@ -0,0 +1,17 @@
+//
+//  A00841625_Activity5App.swift
+//  A00841625_Activity5
+//
+//  Created by Adan González on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct A00841625_Activity5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A00841625_Activity5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A00841625_Activity5
+//
+//  Created by Adan González on 24/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
